b030130158f047592b28633d47631da7471341b7
Jason <Jason@Colins-MacBook-Pro.local>
2018-07-07 15:10:13 -1000

    completed secret number using private  modules

--- secret_number.js
+++ secret_number.js
@@ -1,4 +1,9 @@
 'use strict';
 module.exports = function() {
-  
+    var randomNumber = Math.floor(Math.random()*1000001);
+    console.log(randomNumber)
+    return function() {
+        return randomNumber;
+    }
+    
 };
